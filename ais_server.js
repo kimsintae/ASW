66ab941e6129745d136916aae54782ca1b01a8ca
cjseok <cjseok@218.50.8.165>
2020-03-25 22:34:31 +0900

    # WARNING: head commit changed in the meantime
    test

---
+++ ais_server.js
@@ -0,0 +1,31 @@
+var http = require('http'); 
+var fs = require('fs'); // 파일 읽기, 쓰기 등 을 할 수 있는 모듈 
+// 404 error message : 페이지 오류가 발생했을때, 
+function send404Message(response){ 
+	response.writeHead(404,{"Content-Type":"text/plain"}); 
+	// 단순한 글자 출력 
+	response.write("404 ERROR... "); 
+	response.end(); 
+} 
+// 200 Okay : 정상적인 요청 
+function onRequest(request, response){ 
+	if(request.method === 'GET' && request.url === '/'){ 
+		response.writeHead(200,{"Content-Type":"text/html"}); // 웹페이지 출력 
+		fs.createReadStream("./html/index.html").pipe(response); // 같은 디렉토리에 있는 index.html를 response 함 
+		return;
+	} else if(request.url.lastIndexOf(".css") > -1) {
+		response.writeHead(200,{"Content-Type":"text/css"}); // 웹페이지 출력 
+	} else if(request.url.lastIndexOf(".js") > -1) {
+		response.writeHead(200,{"Content-Type":"text/js"}); // 웹페이지 출력 
+	} else { // file이 존재 하지않을때, 
+		response.writeHead(200,{"Content-Type":"text/html"}); // 웹페이지 출력 
+	}
+	try {
+		fs.createReadStream("./html"+request.url).pipe(response);
+	} catch(e) {
+		send404Message(response); 
+	}
+}
+var server =http.createServer(onRequest);
+server.listen(8888); 
+console.log('Start AIS Server...');
